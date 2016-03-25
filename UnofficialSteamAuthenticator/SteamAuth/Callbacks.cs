c60a28f75a79d750add940dd1529018da82e9312
Thomas Cheyney <webmaster@thomasc.co.uk>
2016-03-25 18:11:06 +0000

    Merge in steamauth instead of submodule

---
+++ UnofficialSteamAuthenticator/SteamAuth/Callbacks.cs
@@ -0,0 +1,17 @@
+﻿using SteamAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamAuth
+{
+    public delegate void FinalizeCallback(AuthenticatorLinker.FinalizeResult result);
+    public delegate void LinkCallback(AuthenticatorLinker.LinkResult response);
+    public delegate void BCallback(bool response);
+    public delegate void FCCallback(Confirmation[] response);
+    public delegate void Callback(string response);
+    public delegate void LoginCallback(LoginResult result);
+    public delegate void LongCallback(long time);
+}
---
+++ UnofficialSteamAuthenticator/SteamAuth/TimeAligner.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+
+namespace SteamAuth
+{
+
+    /// <summary>
+    /// Class to help align system time with the Steam server time. Not super advanced; probably not taking some things into account that it should.
+    /// Necessary to generate up-to-date codes. In general, this will have an error of less than a second, assuming Steam is operational.
+    /// </summary>
+    public class TimeAligner
+    {
+        private static bool _aligned = false;
+        private static int _timeDifference = 0;
+
+        public static void GetSteamTime(IWebRequest web, LongCallback callback)
+        {
+            if (TimeAligner._aligned)
+            {
+                callback(Util.GetSystemUnixTime() + _timeDifference);
+                return;
+            }
+
+            TimeAligner.AlignTime(web, response =>
+            {
+                callback(Util.GetSystemUnixTime() + _timeDifference);
+            });
+        }
+
+        public static void AlignTime(IWebRequest web, BCallback callback)
+        {
+            long currentTime = Util.GetSystemUnixTime();
+            web.Request(APIEndpoints.TWO_FACTOR_TIME_QUERY, "POST", response =>
+            {
+                if (response != null)
+                {
+                    TimeQuery query = JsonConvert.DeserializeObject<TimeQuery>(response);
+                    TimeAligner._timeDifference = (int)(query.Response.ServerTime - currentTime);
+                    TimeAligner._aligned = true;
+
+                    callback(true);
+                } else {
+                    callback(false);
+                }
+            });
+        }
+
+        internal class TimeQuery
+        {
+            [JsonProperty("response")]
+            internal TimeQueryResponse Response { get; set; }
+
+            internal class TimeQueryResponse
+            {
+                [JsonProperty("server_time")]
+                public long ServerTime { get; set; }
+            }
+            
+        }
+    }
+}
